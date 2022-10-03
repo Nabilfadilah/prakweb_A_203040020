e798b403b08cb6842bcf8cdb8280b6c02020cc33
Nabilfadilah <mohammadfadilah00@gmail.com>
2022-10-03 00:06:56 -0700

    Memperbaiki background form ubah data

--- pw/php/ubah.php
+++ pw/php/ubah.php
@@ -26,7 +26,7 @@ if (isset($_POST['ubah'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ubah Data</title>
+    <title>Tambah Data</title>
 
     <!--Import Google Icon Font-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
