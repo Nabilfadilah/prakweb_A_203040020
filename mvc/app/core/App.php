4af9d2cdc58371e0bd1e24a17c459b78c3e054ed
Nabilfadilah <mohammadfadilah00@gmail.com>
2022-10-07 21:20:14 +0700

    Menyelesaikan video Membuat Aplikasi MVC dengan PHP #3 Routing

--- mvc/app/core/App.php
+++ mvc/app/core/App.php
@@ -4,6 +4,17 @@ class App
 {
     public function __construct()
     {
-        echo 'OK!';
+        $url = $this->parseURL();
+        var_dump($url);
+    }
+
+    public function parseURL()
+    {
+        if (isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/'); // rtrim = untuk menghilangkan '/'
+            $url = filter_var($url, FILTER_SANITIZE_URL); // membersihkan url-url yang ngaco atau memungkinkan karakter kita di hack
+            $url = explode('/', $url); // explode(delimiter, String) memecah url dengan fungsi explode
+            return $url;
+        }
     }
 }
