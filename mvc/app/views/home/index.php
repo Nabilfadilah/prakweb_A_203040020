d7aa5cb9c047f8a8ca790ac62676754dfa4e3494
Nabilfadilah <mohammadfadilah00@gmail.com>
2022-10-08 15:31:47 +0700

    Menyelesaikan video Membuat Aplikasi MVC dengan PHP #6 Assets

--- mvc/app/views/home/index.php
+++ mvc/app/views/home/index.php
@@ -1,1 +1,10 @@
-<h1>Selamat datang di webiste saya</h1>
+<div class="conteiner">
+
+    <div class="jumbotron mt-4">
+        <h1 class="display-4">Selamat Datang di Website Abdi</h1>
+        <hr class="my-4">
+        <p>Saya saat ini sedang belajar membuat Aplikasi MVC</p>
+        <a href="" class="btn btn-primary btn-lg" role="button">learn more</a>
+    </div>
+
+</div>
